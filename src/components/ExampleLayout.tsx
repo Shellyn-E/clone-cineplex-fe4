10bcbe0f9abf8005c3c4e09eec2676b980723226
Shellyn Euriska <lycorisilytheayeh@gmail.com>
2023-03-21 19:55:10 +0700

    balik ke awal

--- src/components/ExampleLayout.tsx
+++ src/components/ExampleLayout.tsx
@@ -4,7 +4,7 @@ const ExampleLayout = ({ children }: any) => {
   return (
     <>
       <div className="fixed z-50 w-full bg-white h-[64px] p-5 border border-b">
-        Cinema XXI
+        Header
       </div>
       <div className="pt-[88px] pb-4 px-2 md:px-5">{children}</div>
       <div className="w-full bg-gray-50 h-[100px] pb-[48px] md:pb-0 px-2 md:px-5">
