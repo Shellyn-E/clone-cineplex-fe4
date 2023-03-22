614a581d3bec899b8bddb4cfa1ff41d6eddbc3b5
Shellyn Euriska <lycorisilytheayeh@gmail.com>
2023-03-22 21:04:51 +0700

    layouting 1

--- src/components/ExampleLayout.tsx
+++ src/components/ExampleLayout.tsx
@@ -1,11 +1,32 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const ExampleLayout = ({ children }: any) => {
   return (
     <>
-      <div className="fixed z-50 w-full bg-white h-[64px] p-5 border border-b">
-        Cinema XXI
-      </div>
+      <nav className="bg-gray-900">
+        <div className="max-w-100xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-20">
+            <a href="#" className="flex-shrink-0 text-white">
+              <Image
+                src="https://21cineplex.com//theme/v5/assets/img/logo.png"
+                width={100}
+                height={200}
+                alt="Logo"
+              />
+            </a>
+            <div className="ml-10 flex items-baseline">
+              <a
+                href="#"
+                className="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-800 focus:outline-none focus:bg-gray-700"
+              >
+                Home
+              </a>
+              <a href="#"></a>
+            </div>
+          </div>
+        </div>
+      </nav>
       <div className="pt-[88px] pb-4 px-2 md:px-5">{children}</div>
       <div className="w-full bg-gray-50 h-[100px] pb-[48px] md:pb-0 px-2 md:px-5">
         Footer
