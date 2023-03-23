c4f69f7461c00ae4d476c61a7ed1ccdb23a41d8c
Shellyn Euriska <lycorisilytheayeh@gmail.com>
2023-03-23 10:49:49 +0700

    navbar

--- src/components/ExampleLayout.tsx
+++ src/components/ExampleLayout.tsx
@@ -1,33 +1,48 @@
+/* eslint-disable @next/next/no-img-element */
+
 import Image from "next/image";
 import Link from "next/link";
 
 const ExampleLayout = ({ children }: any) => {
   return (
     <>
-      <nav className="bg-gray-900">
-        <div className="max-w-100xl mx-auto px-2 sm:px-6 lg:px-8">
+      <nav>
+        <div className="bg-teal-800 max-w-100xl mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between h-20">
-            <a href="#" className="flex-shrink-0 text-white">
-              <Image
-                src="https://21cineplex.com//theme/v5/assets/img/logo.png"
-                width={100}
-                height={200}
-                alt="Logo"
-              />
-            </a>
-            <div className="ml-10 flex items-baseline">
-              <a
-                href="#"
-                className="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-800 focus:outline-none focus:bg-gray-700"
-              >
-                Home
+            <div className="absolute inset-y-0 flex items-center">
+              <a href="#" className="flex-shrink-0 text-white">
+                <img
+                  src="https://21cineplex.com//theme/v5/assets/img/logo.png"
+                  width={200}
+                  height={200}
+                  alt="Logo"
+                />
               </a>
             </div>
           </div>
         </div>
       </nav>
-      <div className="pt-[88px] pb-4 px-2 md:px-5">{children}</div>
-      <div className="w-full bg-gray-50 h-[100px] pb-[48px] md:pb-0 px-2 md:px-5">
+      <div className="pt-[20px] pb-4 px-2 md:px-5">
+        <ul>
+          <a href="#" className="p-3">
+            Now Playing
+          </a>
+          <a href="#" className="p-3">
+            Upcoming
+          </a>
+          <a href="#" className="p-3">
+            Theaters
+          </a>
+          <a href="#" className="p-3">
+            Promotion
+          </a>
+          <a href="#" className="p-3">
+            Info 21
+          </a>
+        </ul>
+      </div>
+      <div className="full pt-[13px] pb-4 px-2 md:px-5">{children}</div>
+      <div className="w-full bg-gray-100 h-[100px] pb-[48px] md:pb-0 px-2 md:px-5">
         Footer
       </div>
     </>
